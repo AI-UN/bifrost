a1a5bb188690052d721d0943b0b14dc70afe3a33
Evans Mike <etnperlong@gmail.com>
2026-05-06 16:09:51 +0100

    feat(config): add responses-to-chat compat setting

--- transports/bifrost-http/server/plugins.go
+++ transports/bifrost-http/server/plugins.go
@@ -124,7 +124,13 @@ func loadBuiltinPlugin(ctx context.Context, name string, pluginConfig any, bifro
 		if err != nil {
 			return nil, fmt.Errorf("failed to marshal compat plugin config: %w", err)
 		}
-		return compat.Init(*compatConfig, logger, bifrostConfig.ModelCatalog)
+		return compat.Init(*compatConfig, logger, bifrostConfig.ModelCatalog, func(provider schemas.ModelProvider) *schemas.CustomProviderConfig {
+			cfg, cfgErr := bifrostConfig.GetProviderConfigRaw(provider)
+			if cfgErr != nil || cfg == nil {
+				return nil
+			}
+			return cfg.CustomProviderConfig
+		})
 
 	case modelcatalogresolver.PluginName:
 		return modelcatalogresolver.Init(bifrostConfig.ModelCatalog, logger)
@@ -250,6 +256,7 @@ func (s *BifrostHTTPServer) loadBuiltinPlugins(ctx context.Context) error {
 	compatCfg := &compat.Config{
 		ConvertTextToChat:      cc.ConvertTextToChat,
 		ConvertChatToResponses: cc.ConvertChatToResponses,
+		ConvertResponsesToChat: cc.ConvertResponsesToChat,
 		ShouldDropParams:       cc.ShouldDropParams,
 		ShouldConvertParams:    cc.ShouldConvertParams,
 	}
